d0dd98cd14d8ac960736ed966ef5c2fc5e586cbd
Murphyn5 <nlimurphy@gmail.com>
2023-05-23 11:28:55 -0700

    fixed console errors

--- js/jquery.ripples-min.js
+++ js/jquery.ripples-min.js
@@ -155,7 +155,7 @@
             top: 0,
             right: 0,
             bottom: 0,
-            zIndex: 5
+            zIndex: 0
         }),
         this.$el.addClass("jquery-ripples").append(n),
         this.context = s = n.getContext("webgl") || n.getContext("experimental-webgl"),
